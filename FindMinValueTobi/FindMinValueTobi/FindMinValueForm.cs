2d84c22ae9def223b14acb24e191fddfbbdd89d9
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-11-26 12:51:41 -0500

    p

---
+++ FindMinValueTobi/FindMinValueTobi/FindMinValueForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMinValueTobi
+{
+    public partial class frmFindMinValue : Form
+    {
+        public frmFindMinValue()
+        {
+            InitializeComponent();
+        }
+    }
+}
